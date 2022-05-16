354fc45e2856cf59a3523d076300a4a4ad8e4e47
Nishadatgit <nishadns321@gmail.com>
2022-05-16 18:23:29 +0530

    final commit day 7

--- bin/assignment5.dart
+++ bin/assignment5.dart
@@ -3,6 +3,13 @@ void main(List<String> args) {
 
   List<int> numberList = [1, 5, 7, 89, 34, 56, 78];
 
-  numberList.sort();
-  print('Largset element in the list is :${numberList[numberList.length - 1]}');
+  //numberList.sort();
+  int large = 0;
+  for (int i = 0; i < numberList.length; i++) {
+    if (numberList[i] > large) {
+      large = numberList[i];
+    }
+  }
+  // print('Largset element in the list is :${numberList[numberList.length - 1]}');
+  print('Largset element in the list is: $large');
 }
