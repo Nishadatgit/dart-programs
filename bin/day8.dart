e281e290e44330eaac802de0a85a7fa1cfed6900
Nishadatgit <nishadns321@gmail.com>
2022-05-17 11:48:52 +0530

    assertion commit

---
+++ bin/day8.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  // String username = "nishad";
+  // String psd='admin';
+  // print('Enter username:');
+  // String? user = stdin.readLineSync();
+  // print('Enter password');
+  // String? password=stdin.readLineSync();
+
+  // assert((user == username && psd == password ), "username and password does not match");
+  // print("Login successfull");
+  // int a = 2;
+  // assert((a >= 18), 'error');
+  // print('helllo');
+
+
+}
