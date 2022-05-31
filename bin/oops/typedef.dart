20fe5d6e01d869f246f96fad58e5a9462bb7d617
Nishadatgit <nishadns321@gmail.com>
2022-05-31 12:11:17 +0530

    typedef

---
+++ bin/oops/typedef.dart
@@ -0,0 +1,26 @@
+// typedef maths(int a, int b);
+// add(int a, int b) {
+//   print(a + b);
+// }
+
+// sub(int a, int b) {
+//   print('Sub = ${a - b}');
+// }
+
+// void main(List<String> args) {
+//   maths math = add;
+
+//   math(20, 30);
+// }
+
+typedef  names(String name1,String name2);
+
+combine(String n1,String n2) {
+  print(' welcome $n1 $n2');
+}
+
+void main(List<String> args) {
+  names name;
+  name = combine;
+  name('mohamed', 'nishad');
+}
